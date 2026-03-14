e46925ca2fb27b03d449942fa62ba704a5bf914b
penguinlord-2701 <daksh.a@somaiya.edu>
2026-03-14 17:13:10 +0000

    Fix Terraform security vulnerabilities

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,16 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = "ami-02dfbd4ff395f2a1b"
   instance_type = "t2.micro"
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 
   tags = {
     Name = "DevSecOps-Instance"
   }
 }
-
--- terraform/security.tf
+++ terraform/security.tf
@@ -1,26 +1,28 @@
 resource "aws_security_group" "web_sg" {
-  name = "web_sg"
+  name        = "web_sg"
+  description = "Security group for DevSecOps web app"
 
   ingress {
+    description = "Allow SSH from trusted IP"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]   # SECURITY VULNERABILITY
+    cidr_blocks = ["49.36.123.93/32"]
   }
 
   ingress {
+    description = "Allow Node application traffic"
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+  egress {
+    description = "Allow outbound HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  
 }
-
